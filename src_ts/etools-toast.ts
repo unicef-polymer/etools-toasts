876e10be188f28aefa7085774ef4ddf382ff8c19
Uladzimir Urbanovich <v_urbanovich@mail.ru>
2022-06-15 12:12:52 +0300

    Add etools-toast component

---
+++ src_ts/etools-toast.ts
@@ -0,0 +1,119 @@
+import '@polymer/paper-button/paper-button';
+import {LitElement, html, CSSResultArray, css} from 'lit-element';
+
+export type ToastOptions = {
+  text: string;
+  showCloseBtn: boolean;
+  duration?: number;
+};
+
+export class EtoolsToast extends LitElement {
+  public toastOptions!: ToastOptions;
+  public defaultDuration = 5000;
+
+  render() {
+    return html`
+      <span id="label">${this.toastOptions?.text}</span>
+      ${this.toastOptions?.showCloseBtn
+        ? html`<paper-button @click="${() => this.closeToast()}">Ok</paper-button>`
+        : ''}
+    `;
+  }
+
+  public connectedCallback(): void {
+    super.connectedCallback();
+
+    if (this.isMultiLine()) {
+      this.classList.add('toast-multi-line');
+    }
+
+    if (!this.toastOptions?.showCloseBtn) {
+      setTimeout(() => this.closeToast(), this.toastOptions?.duration || this.defaultDuration);
+    }
+  }
+
+  public closeToast() {
+    this.animate(
+      [
+        {transform: 'translateX(0)', opacity: 1},
+        {transform: 'translateX(-120%)', opacity: 0}
+      ],
+      {duration: 300, easing: 'ease-in'}
+    ).onfinish = () => {
+      this.dispatchEvent(
+        new CustomEvent('toast-closed', {
+          composed: true,
+          detail: this
+        })
+      );
+      this.classList.add('hidden');
+    };
+  }
+
+  private isMultiLine() {
+    const text = this.toastOptions?.text;
+    if (!text) {
+      return false;
+    }
+    return text.length > 80 || text.includes('\n');
+  }
+
+  static get styles(): CSSResultArray {
+    // language=css
+    return [
+      css`
+        :host {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: space-between;
+          max-width: 568px;
+          min-height: 40px;
+          background-color: var(--paper-toast-background-color, #323232);
+          color: var(--paper-toast-color, #f1f1f1);
+          min-width: 288px;
+          padding: 16px 24px;
+          box-sizing: border-box;
+          box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+          border-radius: 2px;
+          margin: 12px;
+          font-size: 14px;
+          cursor: default;
+          animation: show 0.3s;
+          white-space: pre-wrap;
+        }
+        :host(.toast-multi-line) {
+          flex-direction: column;
+          text-align: justify;
+          align-items: flex-start;
+        }
+        :host(.hidden) {
+          opacity: 0;
+        }
+        paper-button {
+          text-transform: uppercase;
+          color: var(--primary-color);
+          padding: 8px;
+          min-width: 16px;
+          margin: 0 -8px 0 24px;
+        }
+        :host(.toast-multi-line) paper-button {
+          margin: 0 -8px -8px 0;
+          align-self: flex-end;
+        }
+        @keyframes show {
+          from {
+            transform: translateY(100px);
+            opacity: 0;
+          }
+          to {
+            transform: translateY(0px);
+            opacity: 1;
+          }
+        }
+      `
+    ];
+  }
+}
+
+window.customElements.define('etools-toast', EtoolsToast);
---
+++ src_ts/etools-toasts.ts
@@ -0,0 +1,68 @@
+import {LitElement, html, CSSResultArray, css} from 'lit-element';
+import {EtoolsToast, ToastOptions} from './etools-toast';
+import {repeat} from 'lit-html/directives/repeat';
+import './etools-toast';
+
+const MAX_TOAST_SHOWN = 3;
+
+export class EtoolsToasts extends LitElement {
+  private toastQueue: ToastOptions[] = [];
+
+  protected render() {
+    return repeat(
+      this.toastQueue.slice(0, MAX_TOAST_SHOWN),
+      (toastOptions: ToastOptions) => toastOptions.text,
+      (toastOptions: ToastOptions) =>
+        html`<etools-toast
+          .toastOptions="${toastOptions}"
+          @toast-closed="${(e: CustomEvent<EtoolsToast>) => this.showNext(e)}"
+        ></etools-toast>`
+    );
+  }
+
+  public connectedCallback() {
+    super.connectedCallback();
+    // @ts-ignore
+    document.body.addEventListener('toast', (event: CustomEvent<ToastOptions>) => this.queueToast(event));
+  }
+
+  public queueToast({detail}: CustomEvent<ToastOptions>) {
+    const alreadyInQueue: boolean = this.toastQueue.some(
+      (toastDetails) => JSON.stringify(toastDetails) === JSON.stringify(detail)
+    );
+    if (alreadyInQueue) {
+      return;
+    }
+
+    this.toastQueue.push(detail);
+    this.requestUpdate();
+  }
+
+  public showNext(event: CustomEvent<EtoolsToast>) {
+    const toastElement: EtoolsToast = event.detail;
+    const index = this.toastQueue.indexOf(toastElement.toastOptions);
+    this.toastQueue.splice(index, 1);
+    toastElement.remove();
+    this.requestUpdate();
+  }
+
+  static get styles(): CSSResultArray {
+    // language=css
+    return [
+      css`
+        :host {
+          position: fixed;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+          padding: 10px;
+          bottom: 0;
+          left: 0;
+          z-index: 10;
+        }
+      `
+    ];
+  }
+}
+
+customElements.define('etools-toasts', EtoolsToasts);
